89068e445baf1954f809b197526142c82adc205f
Dan Fritchman <fritchman@apple.com>
2017-09-11 21:38:52 +0200

    renamed spreadsheet -> sheet, main -> app

--- sheet.py
+++ sheet.py
@@ -10,15 +10,18 @@ client = gspread.authorize(creds)
 sheet_name = 'Sunday Noe Bball Debug'
 sheet = client.open(sheet_name).sheet1
 
+# Load up headers, verify they are as expected
 hdr_names = sheet.row_values(1)
-hdrs = dict(zip(hdr_names, range(len(hdr_names))))
-
-"""
+hdrs = dict(zip(hdr_names, range(1, len(hdr_names)+1)))
 expected_cols = 'Name ID Phone Sunday Quarter '.split()
 for x in expected_cols:
     if x not in hdrs:
         raise ValueError
-"""
+
+# Grab a few lists of columns
+players = sheet.col_values(col=hdrs['Name'])
+ids = sheet.col_values(col=hdrs['ID'])
+phs = sheet.col_values(col=hdrs['Phone'])
 
 # Extract and print all of the values
 list_of_hashes = sheet.get_all_records()
